c85eaffde310557ab63179425f1126d38ad2e935
Asher <124173983+99lash@users.noreply.github.com>
2024-11-26 21:27:01 +0800

    Added Autofill Information in Borrow Functions

--- src/AppController.java
+++ src/AppController.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import custom.utils.Clrscr;
@@ -10,18 +11,18 @@ import custom.utils.Title;
 
 public class AppController {
   private List<BorrowTransaction> transactions;
-  private List<Student> students;
   private List<Account> accounts;
   private List<AuditLog> auditLogs;
+  private StudentController studentController;
   private InventoryController inventoryController;
   private Account currentUser;
   static Scanner in = new Scanner(System.in);
 
   AppController() {
     this.transactions = new ArrayList<>();
-    this.students = new ArrayList<>();
     this.accounts = new ArrayList<>();
     this.auditLogs = new ArrayList<>();
+    this.studentController = new StudentController();
     this.inventoryController = new InventoryController();
   }
 
@@ -30,8 +31,8 @@ public class AppController {
     this.transactions = transactions;
   }
 
-  public void setStudents(List<Student> students) {
-    this.students = students;
+  public void setStudentController(StudentController studentController) {
+    this.studentController = studentController;
   }
 
   public void setAccounts(List<Account> accounts) {
@@ -55,8 +56,8 @@ public class AppController {
     return transactions;
   }
 
-  public List<Student> getStudents() {
-    return students;
+  public StudentController getStudentController() {
+    return studentController;
   }
 
   public List<Account> getAccounts() {
@@ -128,7 +129,7 @@ public class AppController {
 
   // fetch methods
   public void fetchAccounts() {
-    String filepath = "../res/data/accounts/users.csv";
+    String filepath = "../res/data/account/users.csv";
 
     try {
       Scanner reader = new Scanner(new File(filepath));
@@ -145,35 +146,6 @@ public class AppController {
           String password = fields[2];
           String role = fields[3];
           accounts.add(new Account(id, username, password, role));
-        }
-      }
-      reader.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public void fetchStudents() {
-    String filepath = "../res/data/masterlist/BSCS_Masterlist_Orig.csv";
-
-    try {
-      Scanner reader = new Scanner(new File(filepath));
-      boolean header = false;
-      while (reader.hasNextLine()) {
-        String line = reader.nextLine();
-        String[] fields = line.split(",");
-
-        header = fields[0].equalsIgnoreCase("id") ? true : false;
-
-        if (!header) {
-          String lastName = fields[1];
-          String firstName = fields[2];
-          String studentId = fields[3];
-          String course = fields[4];
-          int year = Integer.parseInt(fields[5]);
-          char section = fields[6].charAt(0);
-          String department = fields[7];
-          students.add(new Student(studentId, lastName, firstName, course, year, section, department));
         }
       }
       reader.close();
@@ -201,6 +173,7 @@ public class AppController {
     boolean running = true;
 
     while (running) {
+      new Clrscr();
       new Title();
       System.out.println("1.Borrow an Item");
       System.out.println("2.Return an Item");
@@ -236,6 +209,7 @@ public class AppController {
     boolean running = true;
 
     while (running) {
+      new Clrscr();
       new Title();
       System.out.println("1.Borrow an Item");
       System.out.println("2.Return an Item");
@@ -267,11 +241,95 @@ public class AppController {
 
   }
 
+  public Boolean SubmitVerification(String studentId, String collateral, String borrowed) {
+    studentController.fetchStudentDatabase();
+    Map<Integer, Student> students = studentController.getStudents();
+    Student studentFound = null;
+    char ch = ' ';
+    boolean recordFound = false;
+    while (ch != 'Y' || ch != 'N') {
+      new Clrscr();
+      System.out.println("Review Details");
+      System.out.println("-------------------------------");
+
+      for (Student student : students.values()) {
+        if (student.getStudentId().equalsIgnoreCase(studentId)) {
+          recordFound = true;
+          studentFound = student;
+        }
+      }
+
+      if (recordFound) {
+        System.out.println("Borrowed Resource: " + borrowed);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Full Name: " + studentFound.getFullName());
+        System.out.println("Course/Year/Section: " + studentFound.getCourse() + " - " + studentFound.getYear()
+            + studentFound.getSection());
+        System.out.println("Department: " + studentFound.getDepartment());
+        System.out.println("Collateral: " + collateral);
+        System.out.print("Submit Verification [Y/N]: ");
+        ch = in.nextLine().toUpperCase().charAt(0);
+
+        if (ch == 'Y') {
+          return true;
+        } else if (ch == 'N') {
+          return false;
+        }
+      } else {
+        System.out.println("No Record Found.");
+        new Getch();
+        break;
+      }
+    }
+    return null;
+  }
+
+  public void ComputerForm() {
+    System.out.print("Select Computer Lab (1-3): ");
+    int destination = in.nextInt();
+    inventoryController.initializeComputerList(destination);
+    List<Computer> computers = inventoryController.getComputers();
+
+    System.out.println("\n-------------------------------------------");
+    System.out.print("Select a Computer: ");
+    String pc = "PC" + String.valueOf(in.nextInt());
+    in.nextLine();
+
+    for (Computer computer : computers) {
+      if (computer.getName().equals(pc) && !computer.getStatus().equalsIgnoreCase("available")) {
+        System.out.println("PC is not available at the moment.");
+        new Getch();
+        break;
+      } else if (computer.getName().equals(pc) && computer.getStatus().equalsIgnoreCase("available")) {
+        System.out.print("Enter Student ID: ");
+        String studentId = in.nextLine();
+        System.out.print("Enter Collateral: ");
+        String collateral = in.nextLine();
+        Boolean transactionProccessed = SubmitVerification(studentId, collateral, pc);
+        if (transactionProccessed == null) {
+          continue;
+        } else if (transactionProccessed) {
+          inventoryController.borrowComputer(computer);
+          inventoryController.updateComputerInventory(computers);
+          // BorrowingTransactionController
+          //!LEAVE POINT: Continue where I left of...
+        } else {
+          System.out.println("Borrow Transaction Cancelled.");
+        }
+        new Getch();
+        break;
+      }
+    }
+    computers.clear();
+  }
+
   private void borrowingForm() {
     System.out.println("--Borrowing Form--");
     boolean running = true;
 
     while (running) {
+      new Clrscr();
+      new Title();
       System.out.println("Type of Resources");
       System.out.println("1. PC");
       System.out.println("2. Laptop");
@@ -282,23 +340,7 @@ public class AppController {
 
       switch (ch) {
         case 1:
-          System.out.print("Select Computer Lab (1-3): ");
-          int destination = in.nextInt();
-          inventoryController.initializeComputerList(destination);
-          List<Computer> computers = inventoryController.getComputers();
-
-          System.out.println("\n-------------------------------------------");
-          System.out.print("Select a Computer: ");
-          String pc = "PC" + String.valueOf(in.nextInt());
-
-          for (Computer computer : computers) {
-            if (computer.getName().equals(pc)) {
-              inventoryController.borrowComputer(computer);
-              inventoryController.updateComputerInventory(computers);
-              break;
-            }
-          }
-          computers.clear();
+          ComputerForm();
           running = false;
           break;
 
--- src/Laptop.java
+++ src/Laptop.java
@@ -1,6 +1,18 @@
 public class Laptop extends Resource {
-  
-  Laptop(int id, String name, String type) {
+  String status;
+
+  Laptop(int id, String name, String type, String status) {
     super(id, name, type);
+    setStatus(status);
+  }
+
+  // GETTERS
+  public String getStatus() {
+    return status;
+  }
+
+  // SETTERS
+  public void setStatus(String status) {
+    this.status = status;
   }
 }
